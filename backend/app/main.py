c0b12b0eec456f86d3f87a8043c05c76db093c41
Guggillanithin <217r1a6782@cmrtc.ac.in>
2026-08-14 01:25:18 +0530

    Added vercel Link in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,8 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "https://maple-intranet-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
